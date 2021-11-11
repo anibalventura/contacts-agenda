c25f59a1e7e04d3f615daf63a02243f938287937
Anibal Ventura <anibalventura20@gmail.com>
2021-11-11 12:06:30 -0400

    Add design of login form.

--- ContactAgenda/Form/LoginForm.Designer.cs
+++ ContactAgenda/Form/LoginForm.Designer.cs
@@ -29,13 +29,124 @@ namespace ContactAgenda
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.LblUsername = new System.Windows.Forms.Label();
+            this.LblPassword = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BtnRegister = new System.Windows.Forms.Button();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.TlpMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TlpMain
+            // 
+            this.TlpMain.ColumnCount = 4;
+            this.TlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.TlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.TlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.TlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.TlpMain.Controls.Add(this.LblUsername, 1, 1);
+            this.TlpMain.Controls.Add(this.LblPassword, 1, 2);
+            this.TlpMain.Controls.Add(this.textBox1, 2, 1);
+            this.TlpMain.Controls.Add(this.textBox2, 2, 2);
+            this.TlpMain.Controls.Add(this.BtnRegister, 1, 3);
+            this.TlpMain.Controls.Add(this.BtnLogin, 2, 3);
+            this.TlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TlpMain.Location = new System.Drawing.Point(0, 0);
+            this.TlpMain.Name = "TlpMain";
+            this.TlpMain.RowCount = 4;
+            this.TlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 32.15259F));
+            this.TlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.89646F));
+            this.TlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.07902F));
+            this.TlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40.87193F));
+            this.TlpMain.Size = new System.Drawing.Size(539, 367);
+            this.TlpMain.TabIndex = 0;
+            // 
+            // LblUsername
+            // 
+            this.LblUsername.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LblUsername.AutoSize = true;
+            this.LblUsername.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LblUsername.Location = new System.Drawing.Point(182, 118);
+            this.LblUsername.Name = "LblUsername";
+            this.LblUsername.Size = new System.Drawing.Size(83, 21);
+            this.LblUsername.TabIndex = 0;
+            this.LblUsername.Text = "Username";
+            // 
+            // LblPassword
+            // 
+            this.LblPassword.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LblPassword.AutoSize = true;
+            this.LblPassword.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LblPassword.Location = new System.Drawing.Point(186, 169);
+            this.LblPassword.Name = "LblPassword";
+            this.LblPassword.Size = new System.Drawing.Size(79, 21);
+            this.LblPassword.TabIndex = 1;
+            this.LblPassword.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(271, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox2.Location = new System.Drawing.Point(271, 172);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(128, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // BtnRegister
+            // 
+            this.BtnRegister.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnRegister.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnRegister.Location = new System.Drawing.Point(137, 220);
+            this.BtnRegister.Name = "BtnRegister";
+            this.BtnRegister.Size = new System.Drawing.Size(128, 35);
+            this.BtnRegister.TabIndex = 4;
+            this.BtnRegister.Text = "Register";
+            this.BtnRegister.UseVisualStyleBackColor = true;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnLogin.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnLogin.Location = new System.Drawing.Point(271, 220);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(128, 35);
+            this.BtnLogin.TabIndex = 5;
+            this.BtnLogin.Text = "Login";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(539, 367);
+            this.Controls.Add(this.TlpMain);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.TlpMain.ResumeLayout(false);
+            this.TlpMain.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel TlpMain;
+        private System.Windows.Forms.Label LblUsername;
+        private System.Windows.Forms.Label LblPassword;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button BtnRegister;
+        private System.Windows.Forms.Button BtnLogin;
     }
 }
 
