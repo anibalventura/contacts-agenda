4469574efdb80187ea78cac4a3a8c2103f9ffd27
Anibal Ventura <anibalventura20@gmail.com>
2021-11-11 22:14:25 -0400

    Add user and contact service.

---
+++ BusinessLayer/Service/ContactService.cs
@@ -0,0 +1,42 @@
+﻿using Database.Model;
+using Database.Repository;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BusinessLayer.Service
+{
+    public class ContactService
+    {
+        private ContactRepository contactRepository;
+
+        public ContactService(SqlConnection connection)
+        {
+            contactRepository = new ContactRepository(connection);
+        }
+
+        public bool Add(Contact contact)
+        {
+            return contactRepository.Add(contact);
+        }
+
+        public bool Edit(Contact contact)
+        {
+            return contactRepository.Edit(contact);
+        }
+
+        public bool Delete(int id)
+        {
+            return contactRepository.Delete(id);
+        }
+
+        public Contact GetById(int id)
+        {
+            return contactRepository.GetById(id);
+        }
+
+        public DataTable GetAll()
+        {
+            return contactRepository.GetAll();
+        }
+    }
+}
---
+++ BusinessLayer/Service/UserService.cs
@@ -0,0 +1,31 @@
+﻿using Database.Model;
+using Database.Repository;
+using System.Data.SqlClient;
+
+namespace BusinessLayer.Service
+{
+    public class UserService
+    {
+        private UserRepository userRepository;
+
+        public UserService(SqlConnection connection)
+        {
+            userRepository = new UserRepository(connection);
+        }
+
+        public bool Add(User user)
+        {
+            return userRepository.Add(user);
+        }
+
+        public bool IsRegistered(string username)
+        {
+            return userRepository.IsRegistered(username);
+        }
+
+        public bool Login(string username, string password)
+        {
+            return userRepository.Login(username, password);
+        }
+    }
+}
